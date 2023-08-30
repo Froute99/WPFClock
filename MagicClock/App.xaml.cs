5c15f3e2b04996414af6796b43b5d19acb507801
Froute99 <98374874+Froute99@users.noreply.github.com>
2023-08-30 21:42:27 +0900

    WPF Clock Init

---
+++ MagicClock/App.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Drawing;
+using Winforms = System.Windows.Forms;
+using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace MagicClock
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+		private Winforms.NotifyIcon tray;
+
+		public App()
+		{
+			tray = new Winforms.NotifyIcon();
+		}
+
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			SetTray();
+
+			base.OnStartup(e);
+		}
+
+		private void SetTray()
+		{
+			BitmapImage mainIcon = new BitmapImage(new Uri("pack://application:,,,/Resources/hmm.bmp", UriKind.RelativeOrAbsolute));
+			tray.Icon = Icon.FromHandle(BitmapImage2Bitmap(mainIcon).GetHicon());
+			tray.Visible = true;
+
+
+			BitmapImage exitIcon = new BitmapImage(new Uri("pack://application:,,,/Resources/power.bmp", UriKind.RelativeOrAbsolute));
+			
+			tray.ContextMenuStrip = new Winforms.ContextMenuStrip();
+			tray.ContextMenuStrip.Items.Add("Exit", Image.FromHbitmap(BitmapImage2Bitmap(exitIcon).GetHbitmap()), OnExitClicked);
+		}
+
+		private void OnExitClicked(object sender, EventArgs e)
+		{
+			Shutdown();
+		}
+
+		protected override void OnExit(ExitEventArgs e)
+		{
+			tray.Dispose();
+
+			base.OnExit(e);
+		}
+
+
+		private Bitmap BitmapImage2Bitmap(BitmapImage bitmapImage)
+		{
+			using (MemoryStream outStream = new MemoryStream())
+			{
+				BitmapEncoder enc = new BmpBitmapEncoder();
+				enc.Frames.Add(BitmapFrame.Create(bitmapImage));
+				enc.Save(outStream);
+				System.Drawing.Bitmap bitmap = new System.Drawing.Bitmap(outStream);
+
+				return new Bitmap(bitmap);
+			}
+		}
+
+	}
+}
---
+++ MagicClock/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MagicClock
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		System.Windows.Threading.DispatcherTimer Timer = new System.Windows.Threading.DispatcherTimer();
+		public string Time { get; set; }
+
+		public MainWindow()
+		{
+			InitializeComponent();
+
+
+			Timer.Tick += new EventHandler(Timer_Click);
+			Timer.Interval = new TimeSpan(0, 0, 1);
+			Timer.Start();
+
+		}
+
+		private void Timer_Click(object sender, EventArgs e)
+		{
+			DateTime d;
+			d = DateTime.Now;
+			label1.Content = d.Hour + " : " + d.Minute;
+
+			OutlinedTextLabel(label1);
+		}
+
+		protected override void OnRender(DrawingContext drawingContext)
+		{
+			var location = label1.PointToScreen(new Point(625, -15));
+
+			Window window = GetWindow(label1);
+
+			window.Left = location.X;
+			window.Top = location.Y - window.Height;
+
+
+			base.OnRender(drawingContext);
+		}
+
+
+		void OutlinedTextLabel(Label lbl)
+		{
+			FormattedText formattedText = new FormattedText(lbl.Content.ToString(),
+				System.Globalization.CultureInfo.GetCultureInfo("ko-kr"),
+				FlowDirection.LeftToRight,
+				new Typeface(lbl.FontFamily, lbl.FontStyle, lbl.FontWeight, lbl.FontStretch),
+				lbl.FontSize, Brushes.Black);
+
+			formattedText.TextAlignment = TextAlignment.Center;
+
+			Geometry geometry = formattedText.BuildGeometry(new Point(0, 0));
+
+			PathGeometry pathGeometry = geometry.GetFlattenedPathGeometry();
+
+			Canvas cvs = new Canvas();
+			lbl.Content = cvs;
+
+			Path pt = new Path();
+			pt.Stroke = Brushes.White;
+			pt.StrokeThickness = 1;
+			pt.Fill = lbl.Foreground;
+			pt.Stretch = Stretch.Fill;
+
+			cvs.Children.Add(pt);
+
+			Canvas.SetTop(pt, (cvs.ActualHeight - formattedText.Extent) / 100);
+			Canvas.SetLeft(pt, (cvs.ActualWidth - formattedText.Width) / 100);
+
+			pt.Data = pathGeometry;
+		}
+
+	}
+}
